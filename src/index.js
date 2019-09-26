fa4cef5fc031ad7583210c1912879fbebca549f3
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2019-09-26 07:57:33 -0500

    Use the configured store in the index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+// Configured Store
+import ConfigureStore from './store';
 
 // Container
 import App from './pages/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = ConfigureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
