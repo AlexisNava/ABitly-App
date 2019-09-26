99af197343479d54897a328eaf85427b7a90e56d
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2019-09-26 08:14:52 -0500

    Set up material ui

--- src/index.js
+++ src/index.js
@@ -2,17 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+// MUI Utils
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 // Configured Store
 import ConfigureStore from './store';
 
 // Container
 import App from './pages/App';
 
+// Utils
+import theme from './utils/theme';
+
 const store = ConfigureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <App />
+
+      <CssBaseline />
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById('root'),
 );
