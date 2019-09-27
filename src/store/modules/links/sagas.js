c5b817db6d5c471be9cf7f2f834e4567339d1107
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2019-09-26 19:26:03 -0500

    Add watcherRequestGeneratedLink saga to sagas

---
+++ src/store/modules/links/sagas.js
@@ -0,0 +1,28 @@
+import { takeLatest, put, call } from 'redux-saga/effects';
+
+// Actions
+import { REQUEST_GENERATED_LINK } from './actions';
+
+// Action Creators
+import { requestGeneratedLinkSuccess } from './actionCreators';
+
+// Services
+import { requestGenerateLink } from '../../../services';
+
+function* requestGeneratedLink({ originalURL }) {
+  try {
+    const response = yield call(requestGenerateLink, originalURL);
+
+    console.log('response', response);
+
+    yield put(requestGeneratedLinkSuccess(response));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function* watcherRequestGeneratedLink() {
+  yield takeLatest(REQUEST_GENERATED_LINK, requestGeneratedLink);
+}
+
+export default watcherRequestGeneratedLink;
--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,9 +1,10 @@
 import { all } from 'redux-saga/effects';
 
 // Sagas
+import watcherRequestGeneratedLink from './modules/links/sagas';
 
 function* rootSaga() {
-  yield all([]);
+  yield all([watcherRequestGeneratedLink()]);
 }
 
 export default rootSaga;
