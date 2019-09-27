715a3081135af28c2c3b89ddde66c928054fa76d
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2019-09-26 20:58:23 -0500

    Fix style issues

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
-const { createServer } = require("http");
+require('dotenv').config();
+const { createServer } = require('http');
 
 // Express App
-const app = require("./server/app");
+const app = require('./server/app');
 
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || "0.0.0.0";
+const HOST = process.env.HOST || '0.0.0.0';
 const server = createServer(app);
 
 server.listen(PORT, HOST, () => {
